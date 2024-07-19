033e71f3049fa411fdbe8c5941557b77d67cb1e6
Ryan <coap.ryanmontgomery@gmail.com>
2024-07-19 11:57:57 -0600

    success

--- server/app.py
+++ server/app.py
@@ -1,7 +1,8 @@
 # server/app.py
-#!/usr/bin/env python3
 
-from flask import Flask, make_response
+
+
+from flask import Flask, make_response, g, jsonify
 from flask_migrate import Migrate
 
 from models import db, Earthquake
@@ -14,14 +15,27 @@ app.json.compact = False
 migrate = Migrate(app, db)
 db.init_app(app)
 
-
 @app.route('/')
 def index():
+
     body = {'message': 'Flask SQLAlchemy Lab 1'}
     return make_response(body, 200)
 
-# Add views here
+@app.route('/earthquakes/<int:id>', methods=['GET'])
+def get_earthquake(id):
+    earthquake = Earthquake.query.get(id)
+    if earthquake is None:
+        return jsonify({'message': f'Earthquake {id} not found.'}), 404
+    else:
+        return jsonify(earthquake.to_dict())
 
+@app.route('/earthquakes/magnitude/<float:magnitude>', methods=['GET'])
+def get_mag_greater_than(magnitude):
+    earthquakes = Earthquake.query.filter(Earthquake.magnitude >= magnitude).all()
+    return jsonify({
+        'count': len(earthquakes),
+        'quakes': [quake.to_dict() for quake in earthquakes]
+    })
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
--- server/models.py
+++ server/models.py
@@ -1,9 +1,26 @@
-from flask_sqlalchemy import SQLAlchemy
+
 from sqlalchemy import MetaData
 from sqlalchemy_serializer import SerializerMixin
+from flask_sqlalchemy import SQLAlchemy
 
 metadata = MetaData()
 
 db = SQLAlchemy(metadata=metadata)
 
-# Add models here
+class Earthquake(db.Model, SerializerMixin):
+    __tablename__ = 'earthquakes'
+    id = db.Column(db.Integer, primary_key=True)
+    magnitude = db.Column(db.Float, nullable=False)
+    location = db.Column(db.String, nullable=False)
+    year = db.Column(db.Integer, nullable=False)
+
+    def __repr__(self):
+        return f"<Earthquake {self.id}, {self.magnitude}, {self.location}, {self.year}>"
+
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'magnitude': self.magnitude,
+            'location': self.location,
+            'year': int(self.year)
+        }
